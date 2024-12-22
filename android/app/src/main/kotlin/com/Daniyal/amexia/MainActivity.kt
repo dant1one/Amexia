d14679ff78ac5ccc8f5530a729432eff5d06a26b
daniyal vaahunov <vaahunovdaniyal@gmail.com>
2024-12-22 16:53:15 +0600

    vnfjusv

---
+++ android/app/src/main/kotlin/com/Daniyal/amexia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.Daniyal.amexia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/ait/amexia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ait.amexia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
