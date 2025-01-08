dc07eb794371ce713bf8ceb8cc1b9d3ae7b1fa89
dant1one <vaahunovdaniyal@gmail.com>
2025-01-08 22:23:45 +0600

    operations_system_data_class.dart

--- lib/data_classes_fold/operations_system_data_class.dart
+++ lib/data_classes_fold/operations_system_data_class.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 
 List<OperationsSystemDataClass> operationsSystemDataClassListFromJson(String str) {
   final jsonData = json.decode(str);
-  return List<OperationsSystemDataClass>.from(jsonData.map((x) => OperationsSystemDataClass.fromJson(x)));
+  return List<OperationsSystemDataClass>.from(
+      jsonData.map((x) => OperationsSystemDataClass.fromJson(x))
+  );
 }
 
 String operationsSystemDataClassToJson(OperationsSystemDataClass data) => json.encode(data.toJson());
@@ -26,15 +28,17 @@ class OperationsSystemDataClass {
     required this.total,
   });
 
-  factory OperationsSystemDataClass.fromJson(Map<String, dynamic> json) => OperationsSystemDataClass(
-    id: json["id"],
-    time: DateTime.parse(json["time"]),
-    trade: json["trade"],
-    currency: json["currency"],
-    amount: json["amount"],
-    rate: json["rate"]?.toDouble(),
-    total: json["total"],
-  );
+  factory OperationsSystemDataClass.fromJson(Map<String, dynamic> json) {
+    return OperationsSystemDataClass(
+      id: json["id"],
+      time: DateTime.tryParse(json["time"]) ?? DateTime.now(),
+      trade: json["trade"] ?? '',
+      currency: json["currency"] ?? '',
+      amount: (json["amount"] is num) ? json["amount"].toDouble() : 0.0,
+      rate: (json["rate"] is num) ? json["rate"].toDouble() : 0.0,
+      total: (json["total"] is num) ? json["total"].toDouble() : 0.0,
+    );
+  }
 
   Map<String, dynamic> toJson() => {
     "id": id,
